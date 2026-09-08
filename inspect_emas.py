a97c3549da07d4bf9aee6764caf2df9f4a48d4d6
Wildan Faqihul Futuh <yveslaurentxyz@proton.me>
2026-09-08 12:24:11 +0700

    chore: hapus file temp update perhiasan 8 SEPTEMBER 2026

--- inspect_emas.py
+++
@@ -1,8 +0,0 @@
-import openpyxl
-
-wb = openpyxl.load_workbook('TEMPLATE HARGA MAS2.xlsx', data_only=True)
-ws = wb['EMAS']
-print("Max row:", ws.max_row, "Max col:", ws.max_column)
-for r in range(1, 18):
-    row_vals = [ws.cell(r, c).value for c in range(1, ws.max_column + 1)]
-    print(f"Row {r:2d}: {row_vals}")
